604cc326f27fecac739d64b7be732ef21ef34377
gmr90 <govardhanreddy90@gmail.com>
2017-05-31 20:05:34 -0400

    add: Angular events code

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+//AngularJS modules define applications
+var app = angular.module('myApp',[])
+//AngularJS controllers control applications
+app.controller('myCtrl',function($scope){
+
+	$scope.firstName = "gmr";
+	$scope.lastName = "90";
+});
